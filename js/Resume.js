a72061cfaaea3659be649ef370acc5ed7d53e081
Fal2e <115502552+Fal2e@users.noreply.github.com>
2022-12-07 15:39:24 -0800

    Add files via upload

---
+++ js/Resume.js
@@ -0,0 +1,53 @@
+window.onload = () => {
+	
+	let all = document.getElementsByClassName("zoomD"),
+		lightbox = document.getElementById("lightbox");
+   
+	
+	if (all.length>0) { for (let i of all) {
+	  i.onclick = () => {
+		let clone = i.cloneNode();
+		clone.className = "";
+		lightbox.innerHTML = "";
+		lightbox.appendChild(clone);
+		lightbox.className = "show";
+	  };
+	}}
+   
+
+	lightbox.onclick = () => {
+	  lightbox.className = "";
+	};
+  };
+
+//Slides
+
+
+  let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
